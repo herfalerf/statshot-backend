16eb20c3c46e382ef95b0a56c4ec0f3e5f918570
unknown <jpendy@gmail.com>
2021-04-19 23:16:40 -0500

    added length validator to username and password on registration form

--- forms.py
+++ forms.py
@@ -1,12 +1,12 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, IntegerField
-from wtforms.validators import InputRequired
+from wtforms.validators import InputRequired, Length
 
 class RegisterForm(FlaskForm):
     """Form for registering a user."""
 
-    username = StringField("username", validators=[InputRequired()])
-    password = PasswordField("password", validators=[InputRequired()])
+    username = StringField("username", validators=[InputRequired(), Length(min=4, max=25)])
+    password = PasswordField("password", validators=[InputRequired(), Length(min=4, max=25)])
 
 class LoginForm(FlaskForm):
     """Form for validating user login"""
